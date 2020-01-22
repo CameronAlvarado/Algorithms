16b868b9ac3248bc73f2690034b31ad1412dbf4a
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-22 15:48:01 -0800

    done with stock prices and recipies

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,31 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  
+	# if ingredient ammounts are LESS than those in the recipie ammounts, return 0
+	if len(ingredients) < len(recipe):
+		return 0
+
+	# if ingredient ammounts are EQUAL TO or MORE, do division
+	else:
+		recipe_vals = []
+		ingredient_vals = []
+
+		for item in recipe:
+			recipe_vals.append(recipe[item])
+
+		for item in ingredients:
+			ingredient_vals.append(ingredients[item])
+
+		for x in recipe_vals:
+			for y in ingredient_vals:
+				z = y // x
+				return z
+
+# recipe_batches({ 'milk': 100, 'flour': 4, 'sugar': 10, 'butter': 5 }, { 'milk': 1288, 'flour': 9, 'sugar': 95 }), 0
+# recipe_batches({ 'milk': 100, 'butter': 50, 'cheese': 10 }, { 'milk': 198, 'butter': 52, 'cheese': 10 }), 1
+# recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }), 2
+# recipe_batches({ 'milk': 2 }, { 'milk': 200}), 100
 
 
 if __name__ == '__main__':
