03b9e67c8660d233cce8adab2406e3ca29fd7e42
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-23 11:00:10 -0800

    added time calculations

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,24 +1,57 @@
-#!/usr/bin/python
+# #!/usr/bin/python
 
-import sys
+# import sys
 
-# The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
-# recursive solution
+# # The cache parameter is here for if you want to implement
+# # a solution that is more efficient than the naive 
+# # recursive solution
+# def eating_cookies(n, cache=None):
+# 	if cache is None:
+# 		cache = {0: 1, 1: 1, 2: 2}
+# 	if n < 0:
+# 		return 0
+# 	elif n not in cache:
+# 		cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+# 	return cache[n]
+
+# # print(eating_cookies(3))
+
+import datetime
+
+start = datetime.datetime.now()
+
+# def eating_cookies(n, cache=None):
+# 	if n < 0:
+# 		return 0
+# 	elif n == 0:
+# 		return 1
+# 	else:
+# 		result = eating_cookies(n - 3) + eating_cookies(n - 2) + eating_cookies(n - 1)
+# 		end = datetime.datetime.now()
+# 		total = end - start
+# 		print(total)
+
+# 		return result
+
+# print(eating_cookies(10))
+
 def eating_cookies(n, cache=None):
-	if cache is None:
-		cache = {0: 1, 1: 1, 2: 2}
+	if cache is None or type(cache) == list:
+		cache = {0:1, 1:1, 2:2, 3:4}
 	if n < 0:
 		return 0
 	elif n not in cache:
-		cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+		cache[n] = eating_cookies(n - 3, cache) + eating_cookies(n - 2, cache) + eating_cookies(n - 1, cache)
+		end = datetime.datetime.now()
+		total = end - start
+		print(total)
 	return cache[n]
 
-# print(eating_cookies(3))
+print(eating_cookies(10))
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
