050c9c3f4ae506a6212865ecd61b633a9b8779af
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-23 14:30:28 -0800

    done wirh rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,8 +3,16 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
-
+  rps = ['rock', 'paper', 'scissors']
+  combinations = [] 
+    # from lecture, ensure the arguments coming in are expected types
+  def helper_function(n: int, result: list): # not needed, just defines types
+    if n == 0:
+      return combinations.append(result)
+    for y in rps:
+      helper_function(n - 1, result + [y])
+  helper_function(n, [])
+  return combinations
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
