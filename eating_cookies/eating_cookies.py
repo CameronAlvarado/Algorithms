5f21fda43e7cfcf82d626d383f6e189becdc1af0
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2020-01-22 18:07:29 -0800

    done with cookie recursion

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -6,11 +6,20 @@ import sys
 # a solution that is more efficient than the naive 
 # recursive solution
 def eating_cookies(n, cache=None):
-  pass
+  if n < 0:
+  	# print("n < 0:", n)
+  	return 0
+  elif n == 0:
+  	# print("n == 0:", n)
+  	return 1 # <-- ???
+  # print("n > 0:", n)
+  return eating_cookies(n-3) + eating_cookies(n-2) + eating_cookies(n-1)
+  
+print(eating_cookies(3))
 
-if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+# if __name__ == "__main__":
+#   if len(sys.argv) > 1:
+#     num_cookies = int(sys.argv[1])
+#     print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+#   else:
+#     print('Usage: eating_cookies.py [num_cookies]')
